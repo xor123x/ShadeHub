35b53469ffcd588b3e8d35b5f0c79300b35ec5a5
xor123x <77859608+xor123x@users.noreply.github.com>
2021-05-19 10:50:25 -0400

    Update test.lua

--- Scripts/test.lua
+++ Scripts/test.lua
@@ -5,11 +5,18 @@ local Main = Library.CreateLib("ShadeHub - Arsenal", "Ocean")
 --tabs
 local Combat = Main:NewTab("Combat")
 local Visuals = Main:NewTab("Visuals")
+local Misc = Main:NewTab("Misc")
+local Credits = Main:NewTab("Credits")
 
 --Sections
 local cbSection = Combat:NewSection("Combat")
 local vSection = Visuals:NewSection("Visuals")
+local mSection = Misc:NewSection("Misc")
+local mpSection = Misc:NewSection("Player")
+local cSection = Credits:NewSection("Credits")
 
+
+--toggles
 cbSection:NewToggle("SilentAim", "Enables SilentAim.", function(state)
     if state then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/xor123x/arsenal-script-testing-kavoui/master/silentaim"))()
@@ -18,10 +25,31 @@ cbSection:NewToggle("SilentAim", "Enables SilentAim.", function(state)
     end
 end)
 
-vSection:NewToggle("Esp", "Enables Esp.", function(state)
-    if state then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/xor123x/arsenal-script-testing-kavoui/master/boxesp"))()
-    else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/xor123x/arsenal-script-testing-kavoui/master/toggleoff"))()
-    end
+
+--buttons
+vSection:NewButton("Box esp", "Esp.", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xor123x/arsenal-script-testing-kavoui/master/boxesp"))()
 end)
+
+mSection:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.F, function()
+	Library:ToggleUI()
+end)
+
+cSection:NewButton("Uilib - xHeptc", "Maker of gui lib", function()
+    print("TY xHeptc")
+end)
+
+cSection:NewButton("Scripting - Shade/tim", "Owner of script", function()
+    print("ShadeHubOnTop")
+end)
+
+--sliders
+mpSection:NewSlider("WalkSpeed", "Changes walk speed.", 50, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+mpSection:NewSlider("JumpPower", "Changes Jumper Power.", 100, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.jumppower = s
+end)
+
+--dropdowns
