37ba5715116d6b1e0c131406be0b48d14e00bc39
xor123x <77859608+xor123x@users.noreply.github.com>
2021-05-19 08:22:50 -0400

    Update test.lua

--- Scripts/test.lua
+++ Scripts/test.lua
@@ -8,12 +8,20 @@ local Visuals = Main:NewTab("Visuals")
 
 --Sections
 local cbSection = Combat:NewSection("Combat")
-local vSection = Visuals:NewSection("Combat")
+local vSection = Visuals:NewSection("Visuals")
 
 cbSection:NewToggle("SilentAim", "Enables SilentAim.", function(state)
     if state then
         print("Toggle On")
     else
         print("Toggle Off")
+    end
+end)
+
+vSection:NewToggle("Esp", "Enables Esp.", function(state)
+    if state then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xor123x/arsenal-script-testing-kavoui/master/boxesp"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xor123x/arsenal-script-testing-kavoui/master/toggleoff"))()
     end
 end)
