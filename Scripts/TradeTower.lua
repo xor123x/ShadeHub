6018edd221f2efe4dc8a0af5c047ee1fd85261d6
xor123x <77859608+xor123x@users.noreply.github.com>
2021-05-24 04:13:39 -0400

    Update TradeTower.lua

--- Scripts/TradeTower.lua
+++ Scripts/TradeTower.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-    local Window = Library.CreateLib("ShadeHub - Arsenal", "Ocean")
+    local Window = Library.CreateLib("ShadeHub - TradeTower", "Ocean")
  
     -- MAIN
     local Main = Window:NewTab("AutoFarm")
