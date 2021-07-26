402d57c2ad5f2176a6663bed8fdc25ea710f9edd
xor123x <77859608+xor123x@users.noreply.github.com>
2021-07-26 03:09:35 -0400

    Update Arsenal.lua

--- Scripts/Arsenal.lua
+++ Scripts/Arsenal.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-    local Window = Library.CreateLib("ShadeHub - Arsenal", "Ocean")
+    local Window = Library.CreateLib("SoriaHub - Arsenal", "Ocean")
  
 
 local Combat = Window:NewTab("Combat")
