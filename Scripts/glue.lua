991f161e16b0ff5330190feffa5b3cd4c58fcebf
xor123x <77859608+xor123x@users.noreply.github.com>
2021-07-26 03:13:31 -0400

    Update glue.lua

--- Scripts/glue.lua
+++ Scripts/glue.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
-local Window = Library.CreateLib("ShadeHub - Glue Piece", "Ocean")
+local Window = Library.CreateLib("SoriaHub - Glue Piece", "Ocean")
 
 local Tab = Window:NewTab("AutoFarm")
 
